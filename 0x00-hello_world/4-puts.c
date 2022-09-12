af36a3e5bceb859659ad9636729ee65f0b1a5f57
bmskmike <bmskmike@gmail.com>
2022-09-12 05:31:18 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- * Description: main - prints out a string to stdout.
- * Return: 0 if success.
+ *  Decription: main - Prints out a string to stdout.
+ *  Return: Ø if success.
  */
 int main(void)
 {
-puts("/"Programming is like building a multilingual puzzle");
-return (0);
+puts("/"Programming is like building a multilingual puzzle"
+);
+return (Ø);
 }
